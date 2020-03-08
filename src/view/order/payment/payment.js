f17d8c25afe89e86acf0573df246aee169369e09
yangtz <yangtzl@foxmail.com>
2020-03-08 14:34:33 +0800

    first commit

---
+++ src/view/order/payment/payment.js
@@ -0,0 +1,44 @@
+import orderApi from '@/api/orders'
+import {Button, Cell, CellGroup, NavBar, Radio, RadioGroup, Toast} from 'vant';
+
+export default {
+    components: {
+        [NavBar.name]: NavBar,
+        [Cell.name]: Cell,
+        [CellGroup.name]: CellGroup,
+        [RadioGroup.name]: RadioGroup,
+        [Radio.name]: Radio,
+        [Button.name]: Button,
+        [Toast.name]: Toast
+    },
+
+    data() {
+        return {
+            order: {},
+            payType: 'wxpay'
+        };
+    },
+    mounted() {
+        this.init()
+    },
+    computed: {},
+    methods: {
+        init() {
+            let orderSn = this.$route.query.orderSn
+            let totalPrice = this.$route.query.totalPrice
+            this.order ={orderSn:orderSn,totalPrice:totalPrice}
+        },
+        pay() {
+            let payTypeName = this.payType == 'wxpay' ? '微信支付' : '支付宝'
+            Toast('准备使用' + payTypeName + '支付')
+            orderApi.payment(this.order.orderSn,this.payType).then(response => {
+                console.log('payment',response)
+            })
+            this.$router.push('/')
+        },
+        formatPrice(price) {
+            return (price / 100).toFixed(2);
+        }
+
+    }
+};
---
+++ src/view/user/user.js
@@ -0,0 +1,37 @@
+import userApi from '@/api/user'
+import {Cell, CellGroup, Col, Icon, Row, Tabbar, TabbarItem, Toast} from 'vant';
+
+export default {
+    components: {
+        [Row.name]: Row,
+        [Col.name]: Col,
+        [Icon.name]: Icon,
+        [Cell.name]: Cell,
+        [CellGroup.name]: CellGroup,
+        [Tabbar.name]: Tabbar,
+        [TabbarItem.name]: TabbarItem,
+        [Toast.name]: Toast
+    },
+    data() {
+        return {
+            activeFooter: 3
+        }
+    },
+    mounted() {
+        this.init()
+    },
+    methods: {
+        init() {
+            userApi.getUserInfo().then(response => {
+            }).catch((err) => {
+                this.$router.replace({path:'login',query:{redirect:'user'}})
+            })
+        },
+        sorry(){
+          Toast('敬请期待')
+        },
+        toOrder(status){
+            this.$router.push({path:'order',query:{status:status}})
+        }
+    }
+}
